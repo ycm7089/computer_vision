6f5144c60001d26af775069fa69cfec95cbbdd13
ycm7089 <ycm7089>
2022-07-08 14:52:10 +0900

    convolution

--- src/Convolution.py
+++ src/Convolution.py
@@ -5,19 +5,62 @@ import rospy
 import numpy as np
 from matplotlib import pyplot as plt
 
-def convolution():
-    img1 = cv2.imread('/home/cm/catkin_ws/src/computer_vision/sonsational.jpg')
-    img6 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+def convolution(img):
+    rospy.init_node('convloution_node', anonymous=False)
+    
+    grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('greyscale',img6)
+    # son_img = processImage('Image.jpeg')
+
+    kernel = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    output = convolve2D(grey_img, kernel, padding=2)
+    # cv2.imwrite('ss', output)
+    cv2.imshow('greyscale',output)
     cv2.waitKey(0)
 
+def convolve2D(image, kernel, padding=0, strides=1):
+    kernel = np.flipud(np.fliplr(kernel))
 
+    xKernShape = kernel.shape[0]
+    yKernShape = kernel.shape[1]
+    xImgShape = image.shape[0]
+    yImgShape = image.shape[1]
 
+    xOutput = int(((xImgShape - xKernShape + 2 * padding) / strides) + 1)
+    yOutput = int(((yImgShape - yKernShape + 2 * padding) / strides) + 1)
+    output = np.zeros((xOutput, yOutput))
 
+    if padding !=0:
+        imagePadded = np.zeros((image.shape[0] + padding * 2, image.shape[1] + padding * 2))
+        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
+        print(imagePadded)
+    else:
+        imagePadded = image
+
+    # Iterate through image
+    for y in range(image.shape[1]):
+        # Exit Convolution
+        if y > image.shape[1] - yKernShape:
+            break
+        # Only Convolve if y has gone down by the specified Strides
+        if y % strides == 0:
+            for x in range(image.shape[0]):
+                # Go to next row once kernel is out of bounds
+                if x > image.shape[0] - xKernShape:
+                    break
+                try:
+                    # Only Convolve if x has moved by the specified Strides
+                    if x % strides == 0:
+                        output[x, y] = (kernel * imagePadded[x: x + xKernShape, y: y + yKernShape]).sum()
+                except:
+                    break
+
+    return output
 
 if __name__ == '__main__':
     try:
-        convolution()
+        img = cv2.imread('/home/cm/catkin_ws/src/computer_vision/sonsational.jpg')
+
+        convolution(img)
     except rospy.ROSInterruptException:
         pass
